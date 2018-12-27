83b4c47ca8c7ebf9ab0d2f54843dced6b521dc49
mayank6 <chauhanmayank.621@gmail.com>
2018-12-27 13:33:37 +0530

    efficient solution to check or find the prime number

--- problem7.py
+++ problem7.py
@@ -20,3 +20,43 @@ while(True):
 
 print(a[-1])
 print(len(a))
+
+
+#-------------------------------------------------------------------------------------------------------------#
+#efficient
+import math
+def checkprime(x):
+    if x==1:
+        return False
+    if x==2 or x==3:
+        return True
+    if x%2==0:
+        return False
+    if x%3==0:
+        return False
+    if x<9:
+        return True
+    if x%3==0:
+        return False
+    
+    k=5
+    f=math.floor(math.sqrt(x))
+    
+    while(k<=f):
+        if(x%k==0):
+            return False
+        if(x%(k+2)==0):
+            return False
+        k+=6
+    return True
+y=1    
+a=[]
+x=0
+while(True):
+    y+=1
+    if checkprime(y):
+        a.append(y)
+        x+=1
+    if(x==10001):
+        break
+print(a[-1])
