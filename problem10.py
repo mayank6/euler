b7b8e2f0b77024524ea311a959f90ab29caf0272
mayank6 <chauhanmayank.621@gmail.com>
2018-12-28 12:14:58 +0530

    Update problem10.py
    SieveOfEratosthenes included

--- problem10.py
+++ problem10.py
@@ -32,3 +32,18 @@ print(sum)
 
 #---------------------------------------------------------------------------------------------#
 
+def SieveOfEratosthenes(n): 
+
+    prime = [True for i in range(n+1)] 
+    p = 2
+    while (p * p <= n): 
+        if (prime[p] == True): 
+            for i in range(p * 2, n+1, p): 
+                prime[i] = False
+        p += 1
+    sum=0  
+    for p in range(2, n): 
+        if prime[p]: 
+            sum+=p 
+    print(sum)
+SieveOfEratosthenes(20)  
